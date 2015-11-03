c444d6b7f6f3a500e09389020a487626613aa923
Andrea Bedini <andrea@andreabedini.com>
2015-11-03 12:14:25 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var Promise = require('promise');
+var _ = require('lodash');
+var mjAPI = require("MathJax-node/lib/mj-single.js");
+
+mjAPI.config({
+  MathJax: {
+    SVG: {
+      font: "STIX-Web"
+    }
+  }
+});
+
+mjAPI.start();
+
+var default_output_format = 'svg';
+
+exports.name = 'mathjax';
+exports.outputFormat = default_output_format;
+exports.inputFormats = ['tex'];
+
+exports.renderAsync = function (str, options) {
+  var _opts = _.defaults({}, options, {
+    format: default_output_format
+  });
+
+  return new Promise(function(resolve, reject) {
+    mjAPI.typeset({
+      math: str,
+      format: "TeX", // "inline-TeX", "MathML"
+      mml: _opts.format === 'mml',
+      svg: _opts.format === 'svg'
+    }, function (data) {
+      if (data.errors) {
+        reject(errors)
+      } else {
+        resolve(data[_opts.format]);
+      }
+    })
+  });
+};
+
