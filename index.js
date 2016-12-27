8b3d1970a4f0f36df6ebc4aa4c3878ebcb8b12b2
Andrea Bedini <andrea@andreabedini.com>
2016-12-27 21:38:44 +0800

    Use es6 default arguments rather than lodash

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var _ = require('lodash');
 var mjAPI = require("mathjax-node/lib/mj-single.js");
 
 mjAPI.config({
@@ -17,24 +16,19 @@ exports.name = 'mathjax';
 exports.outputFormat = default_output_format;
 exports.inputFormats = ['tex'];
 
-exports.renderAsync = function (str, options) {
-  var _opts = _.defaults({}, options, {
-    format: default_output_format
-  });
-
+exports.renderAsync = function (str, {format = default_output_format}) {
   return new Promise(function(resolve, reject) {
     mjAPI.typeset({
       math: str,
       format: "TeX", // "inline-TeX", "MathML"
-      mml: _opts.format === 'mml',
-      svg: _opts.format === 'svg'
+      mml: format === 'mml',
+      svg: format === 'svg'
     }, function (data) {
       if (data.errors) {
         reject(errors)
       } else {
-        resolve(data[_opts.format]);
+        resolve(data[format]);
       }
     })
   });
 };
-
